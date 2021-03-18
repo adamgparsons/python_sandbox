82e684e0e4c755cec5794f82cf29968f30843e0a
Adam Parsons <adam@adamparsons.me>
2021-03-18 16:18:20 +0000

    completed files

--- files.py
+++ files.py
@@ -1,1 +1,24 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+# Open a file
+myFile = open('myfile.txt', 'w')
+
+# Get some info
+print('Name ', myFile.name)
+print('Is Closed ', myFile.closed)
+print('Openning Mode', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and Javascript')
+myFile.close()
+
+# Append to a file
+myFile = open('myfile.txt', 'a')
+myFile.write('And I also like CSS')
+myFile.close()
+
+# Read from a file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(10)
+print(text)
